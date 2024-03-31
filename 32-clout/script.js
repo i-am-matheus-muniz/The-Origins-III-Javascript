bd6cd814f95bc50cae74293acf3781b2abbaf26d
Matheus Muniz <i.am.matheus.muniz@gmail.com>
2024-03-31 12:15:18 -0300

    progress into clout exercise

--- 32-clout/script.js
+++ 32-clout/script.js
@@ -1,3 +1,22 @@
 const recentTikTokViews = [1932, 2300, 453, 5222, 6733, 7402, 8334];
 const recentInstagramViews = [936, 2300, 453, 5222, 6733, 7402, 8334];
 const recentYouTubeViews = [989, 2300, 453, 5222, 6733, 7402, 8334];
+
+function mean(viewsArray) {
+    let totalViews = 0;
+    for (let i = 0; i < viewsArray.length; i++){
+        totalViews += viewsArray[i];
+    }
+    console.log("Your total of views is: " + totalViews);
+    console.log("Your array length is: " + viewsArray.length);
+    console.log("The average number of views is: " + (totalViews / viewsArray.length))
+}
+
+function median(viewsArray) {
+    let sortedStats = 0;
+    let middleIndex = 0;
+    
+    viewsArray.sort();
+}
+
+mean(recentTikTokViews);
